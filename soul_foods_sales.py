f3f41bc6359cd98d922a42c8a6f5d9867ecd2999
Tony Bui <z5448625@ad.unsw.edu.au>
2024-12-20 14:14:25 +1100

    region options

--- soul_foods_sales.py
+++ soul_foods_sales.py
@@ -34,21 +34,37 @@ df = pd.read_csv(output_file_path)
 df['date'] = pd.to_datetime(df['date'])
 df_sorted = df.sort_values('date')
 
+regions = ['all'] + list(df['region'].unique())
+
 app.layout = html.Div([
     html.H1("Sales Data Visualizer", style={'textAlign': 'center'}),
     
-    dcc.Graph(id='sales-line-chart')
+    dcc.Graph(id='sales-line-chart'),
+    
+    dcc.Dropdown(
+        id='region-dropdown',
+        options=[{'label': region, 'value': region} for region in regions],
+        value='all',
+        style={'width': '50%'}
+    )
 ])
 
 @app.callback(
     Output('sales-line-chart', 'figure'),
-    [Input('sales-line-chart', 'id')]
+    [Input('region-dropdown', 'value')]
 )
 
-def update_graph(_):
-    fig = px.line(df_sorted, x='date', y='sales', title="Sales Over Time",
+def update_graph(selected_region):
+    if selected_region == 'all':
+       fig = px.line(df_sorted, x='date', y='sales', title=f"Pink Morsel Sales Over Time for {selected_region} Region",
                   labels={'date': 'Date', 'sales': 'Sales'},
                   line_shape='linear')
+    else:
+        df_filtered = df_sorted[df_sorted['region'] == selected_region]
+
+        fig = px.line(df_filtered, x='date', y='sales', title=f"Pink Morsel Sales Over Time for {selected_region} Region",
+                    labels={'date': 'Date', 'sales': 'Sales'},
+                    line_shape='linear')
 
     return fig
 
